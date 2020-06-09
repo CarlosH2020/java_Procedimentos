cf34c3d2fae9c872e322cc6c03cab703ccbd6a32
Carlos Henrique Barros Silva Campos <63325025+CarlosH2020@users.noreply.github.com>
2020-06-08 22:44:22 -0300

    Add files via upload

--- funcao_ex4.java
+++ funcao_ex4.java
@@ -5,6 +5,8 @@
  */
 package funcao;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Root
@@ -15,7 +17,35 @@ public class funcao_ex4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        simbo();
     }
+     public static void  simbo(){
+       Scanner leitor = new Scanner(System.in);
+       int a,b,res;
+       String fun;
+         System.out.println("Digite 2 Valores e a função aritimética( +, -, / , * ).");
+         
+          System.out.print("1N:");
+          a = leitor.nextInt();
+          System.out.print("Função:");
+          fun = leitor.next();
+          System.out.print("2N:");
+          b = leitor.nextInt();
+          if(fun == "+"){
+             res = a + b;
+          }else if(fun == "-"){
+              res = a - b;
+          }else if(fun =="*"){
+            res = a * b;
+          }else if(fun =="/"){
+            res = a / b;
+          }else{
+              System.out.println("Simbolo errado");
+          }
+     }
+       
+       
+       
+     
     
 }
